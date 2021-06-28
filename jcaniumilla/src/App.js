b5d216e5fc153bf0106359444001f4e50a486342
Juan Caniumilla Medina <jcaniumilla@cplt.cl>
2021-06-28 10:29:36 -0400

    first commit

---
+++ jcaniumilla/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src="Juan_BanderaPueblosOriginarios.png" alt="logo" width="25%" />
+        <p>
+          Juan Andrés Caniumilla Medina
+        </p>
+        <h2>Ing. Ejec. en Informática</h2>
+        <h3>Santiago & Coquimbo, Chile.</h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
